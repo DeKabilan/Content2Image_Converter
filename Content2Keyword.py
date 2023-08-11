48153ece8ad060534b5012b0cf9f84c40cf84573
DeKabilan <38588004+DeKabilan@users.noreply.github.com>
2023-08-11 18:46:59 +0530

    Update Content2Keyword.py

--- Content2Keyword.py
+++ Content2Keyword.py
@@ -1,24 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from pyvirtualdisplay import Display
-display = Display(visible=0, size=(800, 800))  
-display.start()
+from selenium.webdriver.chrome.options import Options
 import time
 
 def content2key(content:str):
+    options = Options()
+    options.headless = True
     keywords=[]
-    driver = webdriver.Chrome()
-    driver.get("https://www.cortical.io/freetools/extract-keywords")
-    search_bar = driver.find_element(By.ID, 'text-input')
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://monkeylearn.com/keyword-extractor-online/")
+    search_bar = driver.find_element(By.XPATH, "//textarea")
     search_bar.clear()
     search_bar.send_keys(content)
-    driver.fullscreen_window()
-    time.sleep(5)
-    submit = driver.find_element(By.XPATH, "//button[contains(@id, 'submit-text')]")
+    submit = driver.find_element(By.XPATH, "//button[contains(@type, 'button')]")
     submit.click()
-    time.sleep(5)
-    tags = driver.find_elements(By.XPATH, "//span[contains(@class, 'demo-keyword')]")
+    time.sleep(3)
+    tags = driver.find_elements(By.XPATH, "//span[contains(@class, 'EmbedModel-module--green--aXVCp')]")
     for results in tags:
         keywords.append(results.text)
     driver.close()
